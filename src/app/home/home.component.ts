3f410ebaa587fbf2b7d47480b0bf29fe13ada4f9
Frida Jansson <frja2400@student.miun.se>
2025-05-05 16:20:06 +0200

    Skapade en filtreringsfunktion med hjälp av tvåvägs data-bindning och en filterfunktion i min HomeComponent-klass.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { Course } from '../models/course';                  //Importerar mitt interface
 import { HomeService } from '../services/home.service';     //Importerar min service
 import { CommonModule } from '@angular/common';             //Importerar commonmodule för att kunna använda ngFor
+import { FormsModule } from '@angular/forms';               //Importerar modul för formulärhantering
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  //Properties. Denna tomma array fylla på när vi hämtar data från JSON-filen.
+  //Properties.
   courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  filterValue: string = "";
 
   //Skapar en instans av HomeService med dependency injection. 
   //Skapar en privat variabel i klassen som heter homeService av typen HomeService. 
@@ -22,6 +25,15 @@ export class HomeComponent {
     //Anropa metoder i homeService för att hämta datan.
     this.homeService.getCourses().subscribe((courses) => {
       this.courses = courses;
+      this.filteredCourses = courses;   //Startar ofiltrerat
     })
+  }
+
+  applyFilter(): void {
+    this.filteredCourses = this.courses.filter((course) =>
+      //Filtrera på antingen kurskod eller kursnamn.
+      course.code.toLowerCase().includes(this.filterValue.toLowerCase()) ||
+      course.coursename.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
   }
 }
